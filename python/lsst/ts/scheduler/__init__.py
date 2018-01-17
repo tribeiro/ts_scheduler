b1c198d3b18e509970a5f1f79ae2a6b32953d851
Tiago Ribeiro <tiago.astro@gmail.com>
2018-01-17 16:24:26 -0700

    First time building docs

--- python/lsst/ts/scheduler/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Francisco Delgado'
-__email__ = 'fdelgado@lsst.org'
-
-from .version import *
-from .driver import *
-from .main import *
--- python/lsst/ts/scheduler/kernel/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Francisco Delgado'
-__email__ = 'fdelgado@lsst.org'
-
-from .definitions import *
-from .field import *
